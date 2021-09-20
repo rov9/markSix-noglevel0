410017cd2d225f4c3830f4276681afc866dc444c
rov9 <duttasaurabh92@gmail.com>
2021-09-20 20:31:47 +0530

    html,js update

---
+++ main.js
@@ -0,0 +1,31 @@
+var userInput=document.querySelector("#user-input");
+var btnClick=document.querySelector("#btn-Click");
+var outputMessage=document.querySelector("#outputMessage");
+
+var serverURL = "https://api.funtranslations.com/translate/orcish.json";
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function clickHandler() {
+    var inputText = userInput.value; 
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputMessage.innerText = translatedText; })
+        .catch(errorHandler)
+};
+
+
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
+btnClick.addEventListener("click", clickHandler)
+
+
